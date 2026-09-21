7eb2ff56cefe67a363961932a88fa07eb5a6103a
MuhammedAkay <106436149+MuhammedAkay@users.noreply.github.com>
2026-09-21 21:45:47 +0300

    Update PyInstaller command to include assets

--- build_windows.bat
+++ build_windows.bat
@@ -3,5 +3,5 @@ chcp 65001 >nul
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 python -m pip install pyinstaller
-python -m PyInstaller --noconfirm --clean --windowed --name FotoSecim foto_secim.py
+python -m PyInstaller --noconfirm --clean --windowed --name FotoSecim --add-data "assets;assets" foto_secim.py
 pause
